904dff827edb54da472ccba0bcbc2d829ac1832f
Jovain Chisholm <whocodesjs@gmail.com>
2025-03-03 21:06:14 -0500

    When deck of card is clicked, a new card did not add to the list.
    Fixed bug - random card is added to the list when deck of cards is clicked.

--- card-deck/src/components/Deck.js
+++ card-deck/src/components/Deck.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Deck.css";
 
-const Deck = ({ cardsRemaining, onclick }) => {
+const Deck = ({ cardsRemaining, onClick }) => {
   return (
     <div className="deck-container">
       {cardsRemaining > 0 ? (
-        <div className="deck" onClick={onclick}>
+        <div className="deck" onClick={onClick}>
           <div className="deck-back">Deck ({cardsRemaining})</div>
         </div>
       ) : (
